67eb5f407649c4de10603938811452629aed93f6
Chris4496 <62294842+Chris4496@users.noreply.github.com>
2022-11-10 19:24:27 +0800

    Added link to code

--- src/App.svelte
+++ src/App.svelte
@@ -122,9 +122,13 @@
           </h1>
         </div>
         <div class="items-center justify-end md:flex md:flex-1 lg:w-0">
-          <button
+          <a
+            as="button"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Chris4496/VocabSentenceGeneratorFront"
             class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            >View Code</button
+            >View Code</a
           >
         </div>
       </div>
